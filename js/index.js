63af2428040be6025f10dce5c4da5e573de74201
charminggame <62237314+charminggame@users.noreply.github.com>
2020-12-09 01:02:04 +0700

    0.03
    -เพิ่มระบบ search(ไม่สมบูรณ์)

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(function () {
       $("#photo").attr("src", photoUrl);
 
     } else {
-      window.location.href = 'signin.html';
+      window.location.href = 'Signin.html';
     }
   });
 
@@ -80,4 +80,79 @@ function logo() {
               
           });
       })
+}
+
+
+function Searchmanga() {
+  const search = document.getElementById('searchText').value;
+  const rpsearchText = search.replace(/ /g, "");
+  var db = firebase.firestore();
+  $("#Research").empty();
+  db.collection("manga").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const title = doc.data().Name;
+      const rptitlemovie = title.replace(/ /g, "");
+      var card = `
+<div class="card mb-3" style="max-width: 540px;">
+<div class="row no-gutters">
+    <div class="col">
+    <div>
+      <img src="${doc.data().Poster}" class="card-img">
+    </div>
+    </div>
+    <div class="col">
+      <div class="card-body">
+        <h5 class="card-title">${doc.data().Name}</h5>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card-body">
+        <ons-icon icon="md-favorite" size="40px"></ons-icon>
+      </div>
+    </div>
+  </div>
+</div>
+                `;
+      if (rptitlemovie.toLowerCase().indexOf(rpsearchText.toLowerCase()) != -1) {
+        $("#Research").append(card);
+      }
+    });
+  })
+}
+
+function buttonsearch(N) {
+  var db = firebase.firestore();
+  $("#Research").empty();
+  db.collection("manga").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      var Gn = `${doc.data().Number}`
+      var card = `
+<div class="card mb-3" style="max-width: 540px;">
+<div class="row no-gutters" id="s${doc.data().N}">
+<div class="col">
+<div>
+  <img src="${doc.data().Poster}" class="card-img">
+</div>
+</div>
+<div class="col">
+  <div class="card-body">
+    <h5 class="card-title">${doc.data().Name}</h5>
+  </div>
+</div>
+<div class="col">
+  <div class="card-body">
+    <ons-icon icon="md-favorite" size="40px"></ons-icon>
+  </div>
+</div>
+</div>
+</div>
+                `;
+      if (Gn.toLowerCase().indexOf(N) != -1) {
+        $("#Research").append(card);
+      }else{
+        if(N === 0){
+        $("#Research").append(card);
+      }}
+    });
+  })
 }
